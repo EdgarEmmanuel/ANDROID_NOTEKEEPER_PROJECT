7d9f29fe7794e10bbc46783623be063567642265
Edgar <mmnledgar@gmail.com>
2021-05-10 13:30:40 +0000

    making intent to display one item details

--- app/src/main/java/com/example/pluralsight_notekeeper_android/activities/NoteListActivity.java
+++ app/src/main/java/com/example/pluralsight_notekeeper_android/activities/NoteListActivity.java
@@ -1,13 +1,21 @@
 package com.example.pluralsight_notekeeper_android.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pluralsight_notekeeper_android.NoteActivity;
 import com.example.pluralsight_notekeeper_android.R;
+import com.example.pluralsight_notekeeper_android.database.dao.DataManager;
+import com.example.pluralsight_notekeeper_android.database.models.Course;
+import com.example.pluralsight_notekeeper_android.database.models.Note;
 
 import java.util.List;
 
@@ -16,16 +24,40 @@ public class NoteListActivity extends AppCompatActivity {
     ListView list_note;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_note_list);
 
         list_note = findViewById(R.id.list_note);
 
-        initializeListContent();
+        initializeListContentAndHandleClick();
     }
 
-    private void initializeListContent() {
+    private void initializeListContentAndHandleClick() {
+        List<Note> notes = DataManager.getInstance().getNotes();
 
+        ArrayAdapter<Note> notesAdapter =
+                new ArrayAdapter(this,
+                        android.R.layout.simple_list_item_1,
+                        notes);
+
+        list_note.setAdapter(notesAdapter);
+
+        //handle an item click
+
+       //list_note.
+
+        list_note.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position,
+                                    long l) {
+                Intent goToOneItemDetails = new Intent(NoteListActivity.this,
+                        NoteActivity.class);
+
+                startActivity(goToOneItemDetails);
+            }
+        });
     }
+
+
 }
